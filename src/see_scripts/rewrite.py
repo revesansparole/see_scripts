15a8091cfabef49d2f3800069267696039c4a603
revesansparole <revesansparole@gmail.com>
2016-07-21 09:19:58 +0200

    improved rewrite

--- src/see_scripts/rewrite.py
+++ src/see_scripts/rewrite.py
@@ -13,5 +13,21 @@ while i != -1:
     sleep(0.1)
     i = txt.find(pattern)
 
+pattern = "DataFactory("
+nb = len(pattern)
+i = txt.find(pattern)
+while i != -1:
+    txt = txt[:(i - 1)] + 'DF(uid="%s",\n' % uuid1().hex + txt[(i + nb):]
+    sleep(0.1)
+    i = txt.find(pattern)
+
+pattern = "Factory("
+nb = len(pattern)
+i = txt.find(pattern)
+while i != -1:
+    txt = txt[:(i - 1)] + 'Fa(uid="%s",\n' % uuid1().hex + txt[(i + nb):]
+    sleep(0.1)
+    i = txt.find(pattern)
+
 with open("__wralea__.py", 'w') as f:
     f.write(txt)
