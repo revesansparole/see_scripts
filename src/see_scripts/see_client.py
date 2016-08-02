a8ab76d2157e7119b42a9552faa7344944e162a3
revesansparole <revesansparole@gmail.com>
2016-08-02 18:25:53 +0200

    removed b64encode to reflect changes in SEE

--- src/see_scripts/see_client.py
+++ src/see_scripts/see_client.py
@@ -1,7 +1,6 @@
 """Set of python functions to interact with the SEEweb platform.
 """
 
-from base64 import b64encode
 import json
 import os
 import requests
@@ -196,8 +195,6 @@ def register_ro(session, ro_type, ro_def):
         (str): id of registered RO
     """
     loc_def = dict(ro_def)
-    if 'value' in loc_def:
-        loc_def['value'] = b64encode(loc_def['value'])
 
     data = dict(ro_type=ro_type,
                 ro_def=json.dumps(loc_def))
